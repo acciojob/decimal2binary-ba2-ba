f0e442674390f36007730aaafbf180719dafae55
ba2-ba <112476436+ba2-ba@users.noreply.github.com>
2023-07-23 23:09:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let binary="";
+	while(num>0)
+		{
+			let dig =num%2;
+			binary=binary+dig;
+		}
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
